d0ddd251c3ae216db8b963f70f3b5062a8af52dc
Rafael <rafaelbarros.df@gmail.com>
2021-12-23 09:08:13 -0300

    :tada: Initial commit - Projeto e dependências

---
+++ fluxjwt/src/main/java/com/schoolofnet/fluxjwt/FluxjwtApplication.java
@@ -0,0 +1,13 @@
+package com.schoolofnet.fluxjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxjwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxjwtApplication.class, args);
+	}
+
+}
